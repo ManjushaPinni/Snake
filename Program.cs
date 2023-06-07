3dab47cd072c33ea9b4112624d0b0b7faa15c3e5
Manjusha <manjushapinni@gmail.com>
2023-06-08 01:06:00 +1000

    add service to program files

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Builder;
+using Snake.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<ILeaderBoardService, LeaderBoardService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
